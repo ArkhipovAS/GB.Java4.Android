0ed5a7ec6143dda312a5618cf266636de8abaf3e
ArkhipovAS <arhipov.sasha@gmail.com>
2018-10-21 16:42:24 +0300

    add new prj "StarWar"

---
+++ desktop/src/gb/arkhipov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gb.arkhipov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gb.arkhipov.StarWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarWar(), config);
+	}
+}
